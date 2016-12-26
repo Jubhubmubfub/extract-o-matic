6368a48bb2fc7bf8c179906e51ade8ce625cdb2c
Marcus Sakoda <Jubhub@yahool.com>
2016-12-26 08:28:23 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  console.log("document has loaded");
+
+
+  //write a function that extracts a digit from a large number, probably using modulo.
+  //if you have 12345, 0 digit is 5, 1 digit is 4, 2 digit is 3, 4 digit is 2, and 5 digit 
+  function extract(num,digit){
+
+
+
+  }//end of extract function
+
+
+})//end of doucment ready
